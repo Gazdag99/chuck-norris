9e9711bd1c4c418c53ebf104f19be0ecf1c10134
Gazdag Balázs <gazdag.balazs8.a@gmail.com>
2023-06-17 15:54:53 +0200

    Mobile Responsive

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <h1>Chuck Norris Joke Generator</h1>
       <div className='App'>
 
-      <img src="https://www.pngkey.com/png/full/110-1104421_chuck-norris-facts-have-spread-around-the-world.png" alt="" />
+      <img className='chuck'src="https://www.pngkey.com/png/full/110-1104421_chuck-norris-facts-have-spread-around-the-world.png" alt="" />
       <div className="joke">
         <img className='quote' src="https://cdn.pixabay.com/photo/2022/05/29/05/23/quotation-mark-7228448_1280.png" alt="" />
         <p>{chuck}</p>
